12b55ce9888e80fe2b5657a955925c91f1bfc76c
jafar-b <beldarjafar@gmail.com>
2023-06-30 01:28:47 +0530

    -feat added functionalities(create,add,delete,search)

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:todoapp/constants/colors.dart';
 import 'package:todoapp/screens/Todoitems.dart';
 import 'package:todoapp/model/todo.dart';
-          
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -12,16 +12,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  void _handleOnChange(todo Todo){
-    setState((){
-      Todo.isDone=!Todo.isDone;
-    }
-    );
+  final list = todo.list();
+  final _textController = TextEditingController();
+  List<todo> _foundTodos =[];
+  void _handleOnChange(todo todo) {
+    setState(() {
+      todo.isDone = !todo.isDone;
+    });
+  }
+
+  void _onDelete(String id) {
+    setState(() {
+      list.removeWhere((item) => item.id == id);
+    });
+  }
+
+  @override
+  void initState() {
+    _foundTodos = list;
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final list = todo.list();
     return Scaffold(
         backgroundColor: Colors.grey[300],
         appBar: appBar(),
@@ -37,17 +50,19 @@ class _HomeState extends State<Home> {
                       children: <Widget>[
                         Container(
                           margin: const EdgeInsets.only(top: 50, bottom: 20),
-                          child: (Text(
+                          child: (const Text(
                             "All Todos",
                             style: TextStyle(
                                 fontSize: 25.0, fontWeight: FontWeight.w500),
                           )),
                         ),
-                        for (todo Todoo in list)
+                        for (todo Todoo in _foundTodos.reversed)
+                          
                           Todoitems(
                             Todo: Todoo,
-                            onDelete: (){},
-                            onTodoChanged:_handleOnChange ,
+                            onDelete: _onDelete,
+                            onTodoChanged: _handleOnChange,
+                            addTodo: _addTodo,
                           ),
                       ],
                     ))
@@ -63,33 +78,40 @@ class _HomeState extends State<Home> {
                       margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
                       padding:
                           EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                      decoration:  BoxDecoration(
+                      decoration: BoxDecoration(
                         color: Colors.white,
-                        boxShadow: [
+                        boxShadow: const [
                           BoxShadow(
                             color: Colors.grey,
                             offset: Offset(0.0, 0.0),
                             blurRadius: 10.0,
                             spreadRadius: 0.0,
-                          )         ,
+                          ),
                         ],
                         borderRadius: BorderRadius.circular(10),
-                            
                       ),
                       child: TextField(
+                        controller: _textController,
                         decoration: InputDecoration(
                           hintText: 'Add a new Todo',
                           border: InputBorder.none,
                         ),
                       ),
-                    )
-                    ) ,
+                    )),
                     Container(
-                      margin: EdgeInsets.only(bottom: 20,right: 20),
+                      margin: EdgeInsets.only(bottom: 20, right: 20),
                       child: ElevatedButton(
-                        child: Text("+",style: TextStyle(fontSize: 40),),
-                        style: ElevatedButton.styleFrom(backgroundColor: tdBlue,minimumSize: Size(60,60),elevation: 10),
-                        onPressed: (){},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: tdBlue,
+                            minimumSize: Size(60, 60),
+                            elevation: 10),
+                        onPressed: () {
+                          _addTodo(_textController.text);
+                        },
+                        child: const Text(
+                          "+",
+                          style: TextStyle(fontSize: 40),
+                        ),
                       ),
                     )
                   ],
@@ -97,63 +119,87 @@ class _HomeState extends State<Home> {
               )
             ]));
   }
-}
 
+  void _runFilter(String input){
+    List<todo> result = [];
+    if (input.isEmpty) {
+      result = list;
+    } else {
+      result = list
+          .where((item) =>
+              item.TodoText!.toLowerCase().contains(input.toLowerCase()))
+          .toList();
+    }
+    setState(() {
+      _foundTodos=result;
+    });
+  }
 
+  void _addTodo(String text) {
+    setState(() {
+      list.add(todo(
+          id: DateTime.now().millisecondsSinceEpoch.toString(),
+          TodoText: text));
+      _textController.clear();
+    });
+  }
 
-
-Widget searchBox() {
-  return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15.0),
-      // margin: EdgeInsets.symmetric(horizontal: 25.0),
-      child: Center(
-          child: Column(children: [
-        Container(
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(20)),
-          margin: EdgeInsets.symmetric(vertical: 10.0),
-          child: const TextField(
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(0),
-              prefixIcon: Icon(
-                Icons.search,
-                color: tdBlack,
-                size: 20,
-              ),
-              prefixIconConstraints: BoxConstraints(
-                maxHeight: 20,
-                maxWidth: 25,
-              ),
-              border: InputBorder.none,
-              hintText: 'Search',
-            ),
-          ),
-        )
-      ])));
-}
-
-AppBar appBar() {
-  return AppBar(
-      elevation: 5.0,
-      systemOverlayStyle:
-          SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-      backgroundColor: tdBlue,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const Icon(
-            Icons.menu,
-            size: 30,
-          ),
-          Text("Todo App"),
+  Widget searchBox() {
+    
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 15.0),
+        // margin: EdgeInsets.symmetric(horizontal: 25.0),
+        child: Center(
+            child: Column(children: [
           Container(
-            height: 40,
-            width: 40,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(30.0),
-              // child:Image.asset('assets/images/jafar.jpg'),
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            margin: const EdgeInsets.symmetric(vertical: 10.0),
+            child:  TextField(
+                       onChanged: (value)=>{_runFilter(value)},
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(0),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: tdBlack,
+                  size: 20,
+                ),
+                prefixIconConstraints: BoxConstraints(
+                  maxHeight: 20,
+                  maxWidth: 25,
+                ),
+                border: InputBorder.none,
+                hintText: 'Search',
+                
+              ),
             ),
           )
-        ],
-      ));
+        ])));
+  }
+
+  AppBar appBar() {
+    return AppBar(
+        elevation: 5.0,
+        systemOverlayStyle:
+            SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+        backgroundColor: tdBlue,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Icon(
+              Icons.menu,
+              size: 30,
+            ),
+            Text("Todo App"),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30.0),
+                // child:Image.asset('assets/images/jafar.jpg'),
+              ),
+            )
+          ],
+        ));
+  }
 }
--- lib/screens/Todoitems.dart
+++ lib/screens/Todoitems.dart
@@ -1,51 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/constants/colors.dart';
 import 'package:todoapp/model/todo.dart';
+
 class Todoitems extends StatelessWidget {
-final todo Todo;
+  final todo Todo;
   final onTodoChanged;
   final onDelete;
-
-const Todoitems({Key? key, required this.Todo,required ,required this.onTodoChanged,required this.onDelete}) : super(key: key);
-
+  final addTodo;
+  const Todoitems(
+      {Key? key,
+      required this.Todo,
+      required this.onTodoChanged,
+      required this.onDelete,
+      required  this.addTodo,
+      })
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(bottom: 15),
       child: ListTile(
-                      onTap:(){ print("Clicked a todo Item.");},
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20)
-        ),
-        contentPadding: EdgeInsets.symmetric(vertical: 5,horizontal: 20),
-        tileColor: Colors.white,
-
-        leading: Icon(Todo.isDone? Icons.check_box: Icons.check_box_outline_blank,color: tdBlue,),
-        title: Text(Todo.TodoText!,
-          style: TextStyle(fontSize: 16,color: tdBlack,
-            decoration:Todo.isDone? TextDecoration.lineThrough: TextDecoration.none),
-        ),
-          
-        trailing: Container(
-          height: 35,
-          width: 35,
-          
-          padding: EdgeInsets.all( 0),
-          margin: EdgeInsets.symmetric(vertical: 8),
-          decoration: BoxDecoration(
-              color: tdRed,
-              borderRadius: BorderRadius.circular(5)),
-          child: IconButton(
-            onPressed:( ){
-              
-
-            },
-              color:Colors.white,
-              icon:Icon(Icons.delete),
-
-        ),
-        
-      )
-      ),
-    )                                                                                                     ;  }
+          onTap: () {
+            onTodoChanged(Todo);
+          },
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+          tileColor: Colors.white,
+          leading: Icon(
+            Todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+            color: tdBlue,
+          ),
+          title: Text(
+            Todo.TodoText!,
+            style: TextStyle(
+                fontSize: 16,
+                color: tdBlack,
+                decoration: Todo.isDone
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none),
+          ),
+          trailing: Container(
+            height: 35,
+            width: 35,
+            padding: EdgeInsets.all(0),
+            margin: EdgeInsets.symmetric(vertical: 8),
+            decoration: BoxDecoration(
+                color: tdRed, borderRadius: BorderRadius.circular(5)),
+            child: IconButton(
+              onPressed: () {onDelete(Todo.id);},
+              color: Colors.white,
+              icon: const Icon(Icons.delete),
+            ),
+          )),
+    );
+  }
 }
